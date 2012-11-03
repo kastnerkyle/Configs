09b2f92c072fe866a46c4c9f76cfb79211fba2fd
Kyle Kastner <kkastner@Quincy.(none)>
2012-11-02 23:40:45 -0500

    Added working versions for XMonad

---
+++ xmonad.hs
@@ -0,0 +1,17 @@
+import XMonad
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        }

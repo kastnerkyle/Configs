ff3f1c87df1d3cda3cefd3cbacd77c8eae8e2307
Kyle Kastner <kastnerkyle@gmail.com>
2012-11-20 10:24:29 -0600

    Added fix for Java programs, including MATLAB

--- XMonad/xmonad.hs
+++ XMonad/xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
@@ -9,6 +10,7 @@ main = do
     xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
+        , startupHook = setWMName "LG3D"
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc

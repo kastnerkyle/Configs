b30baacb48e3e005283af2c48ea8c5b6adeed5e6
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-05-20 21:24:11 -0500

    Updated emacs config to quit everything

--- Emacs/init.el
+++ Emacs/init.el
@@ -15,3 +15,11 @@
       (cons
        '("\\.m$" . octave-mode)
        auto-mode-alist))
+;;; esc quits;
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)

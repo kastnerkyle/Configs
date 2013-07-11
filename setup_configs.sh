f3585c0570c4446d7678fa7f0282719cde96e002
Kyle Kastner <kkastner@red.(none)>
2013-07-11 11:12:58 -0500

    Added conditional colorcolunm for CentOS

---
+++ setup_configs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CUR_DIR=`pwd`
+mkdir -p $HOME/.vim/plugin
+ln -s $CUR_DIR/Vim/word_complete.vim $HOME/.vim/plugin/
+ln -s $CUR_DIR/Vim/vimrc $HOME/.vimrc
+
+mkdir ~/.xmonad
+ln -s $CUR_DIR/XMonad/xmonad.hs $HOME/.xmonad/xmonad.hs
+ln -s $CUR_DIR/XMonad/xmobarrc $HOME/.xmobarrc
+if grep -q "THEANO_FLAGS" $HOME/.bashrc; then
+    echo "THEANO_FLAGS is already defined"
+else
+	echo 'export THEANO_FLAGS="mode=FAST_RUN,floatX=float32,device=gpu"' >> $HOME/.bashrc
+fi
+if grep -q "PYLEARN2_DATA_PATH" $HOME/.bashrc; then
+    echo "PYLEARN2_DATA_PATH is already defined"
+else
+	echo 'export PYLEARN2_DATA_PATH="/home/kkastner/pylearn2_data"' >> $HOME/.bashrc
+fi

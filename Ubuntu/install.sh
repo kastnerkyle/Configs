030a0a7401b8605c3d223c670e63542816aa47d0
Kyle Kastner <kastnerkyle@gmail.com>
2013-09-17 13:34:05 -0500

    Added wireless drivers for 5100 issues

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -4,3 +4,5 @@ sudo apt-get dist-upgrade
 sudo apt-get install linux-source linux-headers-`uname -r`
 sudo apt-get install xmonad xmobar suckless-tools trayer build-essential xclip openssh-server screen valgrind vim scrot flashplugin-installer nvidia-cuda-toolkit nvidia-current-updates
 
+#5100 drivers for T400  http://wireless.kernel.org/en/users/Drivers/iwlwifi/?n=downloads#Download
+#http://kuscsik.blogspot.com/2007/06/how-to-install-intel-4965-wireless.html

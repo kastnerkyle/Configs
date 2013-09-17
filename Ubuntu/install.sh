f0aaae05325d9c6fb0acd994b385ff6ec69341a7
Kyle Kastner <kastnerkyle@gmail.com>
2013-09-17 13:00:20 -0500

    Removed texlive from install script (so slow!)

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install linux-source linux-headers-`uname -r`
-sudo apt-get install xmonad xmobar suckless-tools trayer texlive-full build-essential xclip openssh-server screen valgrind vim scrot flashplugin-installer nvidia-cuda-toolkit nvidia-current-updates
+sudo apt-get install xmonad xmobar suckless-tools trayer build-essential xclip openssh-server screen valgrind vim scrot flashplugin-installer nvidia-cuda-toolkit nvidia-current-updates
 

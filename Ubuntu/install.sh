7d1d3ca161cfba5f797aa784d21eec1e128132a3
Kyle Kastner <kastnerkyle@gmail.com>
2013-11-14 19:52:16 -0600

    Added Anaconda installer to automatic

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install linux-source linux-headers-`uname -r`
-sudo apt-get install xmonad xmobar suckless-tools trayer build-essential xclip openssh-server screen valgrind vim scrot flashplugin-installer nvidia-cuda-toolkit nvidia-current-updates
-sudo apt-get install python-pip
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y linux-source linux-headers-`uname -r`
+sudo apt-get install -y xmonad xmobar suckless-tools trayer build-essential xclip openssh-server screen valgrind vim scrot flashplugin-installer nvidia-cuda-toolkit nvidia-current-updates
+sudo apt-get install -y python-pip
+wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.8.0-Linux-x86_64.sh
+bash Anaconda-1.8.0-Linux-x86_64.sh -b
+echo "export PATH=$HOME/anaconda/bin:"'$PATH' >> ~/.bashrc
 
 #5100 drivers for T400  http://wireless.kernel.org/en/users/Drivers/iwlwifi/?n=downloads#Download
 #http://kuscsik.blogspot.com/2007/06/how-to-install-intel-4965-wireless.html

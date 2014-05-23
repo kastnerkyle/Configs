dbd5d8845e72e74b24873299db2a6f41b20dafb9
Kyle Kastner <kastnerkyle@gmail.com>
2014-05-22 19:35:08 -0500

    Added new Anaconda to Ubuntu wget script

--- Octave/install.sh
+++ Octave/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-add-repository ppa:octave/stable
+sudo apt-add-repository ppa:octave/unstable
 sudo apt-get update
 sudo apt-get install octave
--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -6,8 +6,10 @@ sudo apt-get install -y xmonad xmobar suckless-tools trayer build-essential xcli
 sudo apt-get install -y acpi
 sudo sed -i 's:. /usr/share/acpi-support/screenblank:echo -n mem > /sys/power/state:' /etc/acpi/lid.sh
 sudo apt-get install -y python-pip
-wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.8.0-Linux-x86_64.sh
-bash Anaconda-1.8.0-Linux-x86_64.sh -b
+#wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.8.0-Linux-x86_64.sh
+#bash Anaconda-1.8.0-Linux-x86_64.sh -b
+wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.9.2-Linux-x86_64.sh
+bash Anaconda*.sh
 echo "export PATH=$HOME/anaconda/bin:"'$PATH' >> ~/.bashrc
 
 #5100 drivers for T400  http://wireless.kernel.org/en/users/Drivers/iwlwifi/?n=downloads#Download

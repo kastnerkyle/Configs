71ab9cebc838de080ac501b5f62e445b04ba2f46
Kyle Kastner <kastnerkyle@gmail.com>
2014-08-08 13:56:28 +0200

    Added partial setup scripts for pylearn2 and theano

---
+++ Theano+Pylearn2/anaconda2.sh
@@ -0,0 +1,4 @@
+# added by Anaconda 2.0.1 installer
+export PATH="/volatile/accounts/kkastner/anaconda/bin:$PATH"
+export PYTHONPATH="$HOME/anaconda/lib/python2.7/site-packages":"$HOME"
+export PYLEARN2_DATA_PATH="$HOME/pylearn2_data"
--- Theano+Pylearn2/setup_configs.sh
+++ Theano+Pylearn2/setup_configs.sh
@@ -1,11 +1,38 @@
-#!/bin/bash
-if grep -q "THEANO_FLAGS" $HOME/.bashrc; then
-    echo "THEANO_FLAGS is already defined"
-else
-	echo 'export THEANO_FLAGS="mode=FAST_RUN,floatX=float32,device=gpu"' >> $HOME/.bashrc
-fi
-if grep -q "PYLEARN2_DATA_PATH" $HOME/.bashrc; then
-    echo "PYLEARN2_DATA_PATH is already defined"
-else
-	echo 'export PYLEARN2_DATA_PATH="/home/kkastner/pylearn2_data"' >> $HOME/.bashrc
-fi
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
+#sudo apt-get -y dist-upgrade
+#sudo apt-get -y install git make python-dev python-setuptools libblas-dev gfortran g++ python-pip python-numpy python-scipy liblapack-dev
+#sudo pip install ipython nose
+#sudo apt-get install screen
+sudo pip install --upgrade git+git://github.com/Theano/Theano.git
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_5.5-0_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1204_5.5-0_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda
+#THEANO_FLAGS=floatX=float32,device=gpu0 python /usr/local/lib/python2.7/dist-packages/theano/misc/check_blas.py
+cd $HOME
+git clone git://github.com/lisa-lab/pylearn2.git
+cd pylearn2
+sudo python setup.py install
+cd ..
+echo "export PATH=/usr/local/cuda-5.5/bin:$PATH" >> .bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-5.5/lib64:$LD_LIBRARY_PATH" >> .bashrc
+echo "export PYLEARN2_DATA_PATH=/home/ubuntu/data" >> .bashrc
+source .bashrc
+mkdir -p $HOME/pylearn2_data/mnist/
+cd $HOME/pylearn2_data/mnist/
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+gunzip train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+gunzip train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+gunzip t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+gunzip t10k-labels-idx1-ubyte.gz
+cd $HOME
+echo '[global]
+floatX = float32
+device = gpu0
+ 
+[nvcc]
+fastmath = True' > .theanorc

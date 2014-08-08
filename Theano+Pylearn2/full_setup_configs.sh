b660a80bafaf00f7f0c99f62b8a3d6deaa133464
Kyle Kastner <kastnerkyle@gmail.com>
2014-08-08 16:13:51 +0200

    First pass at total installer

--- Theano+Pylearn2/full_setup_configs.sh
+++ Theano+Pylearn2/full_setup_configs.sh
@@ -1,24 +1,28 @@
-#sudo apt-get -y update
-#sudo apt-get -y upgrade
-#sudo apt-get -y dist-upgrade
-#sudo apt-get -y install git make python-dev python-setuptools libblas-dev gfortran g++ python-pip python-numpy python-scipy liblapack-dev
-#sudo pip install ipython nose
-#sudo apt-get install screen
-sudo pip install --upgrade git+git://github.com/Theano/Theano.git
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install screen vim
+wget http://repo.continuum.io/archive/Anaconda-2.0.0-Linux-x86_64.sh
+bash Anaconda*.sh -b
+echo "export PATH=$HOME/anaconda/bin:"'$PATH' >> $HOME/.bashrc
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_5.5-0_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1204_5.5-0_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda
 #THEANO_FLAGS=floatX=float32,device=gpu0 python /usr/local/lib/python2.7/dist-packages/theano/misc/check_blas.py
-cd $HOME
+cd $HOME/src
 git clone git://github.com/lisa-lab/pylearn2.git
+git clone git://github.com/Theano/Theano.git
 cd pylearn2
-sudo python setup.py install
+python setup.py develop 
 cd ..
+cd Theano
+python setup.py develop
+cd $HOME
 echo "export PATH=/usr/local/cuda-5.5/bin:$PATH" >> .bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-5.5/lib64:$LD_LIBRARY_PATH" >> .bashrc
 echo "export PYLEARN2_DATA_PATH=/home/ubuntu/data" >> .bashrc
-source .bashrc
+source $HOME/.bashrc
 mkdir -p $HOME/pylearn2_data/mnist/
 cd $HOME/pylearn2_data/mnist/
 wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz

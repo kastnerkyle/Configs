b9def865e93f739eb022271548b54391827aa774
Kyle Kastner <kastnerkyle@gmail.com>
2014-08-26 06:42:39 -0500

    Added more xrandr tricks

--- xrandr_scripts/presentation_mode.sh
+++ xrandr_scripts/presentation_mode.sh
@@ -3,9 +3,11 @@
 xrandr --auto --output VGA1 --same-as LVDS1
 #xrandr --output VGA1 --mode 1280x1024 --same-as LVDS1
 #xrandr --output VGA1 --off
+#xrandr --output VGA1 --mode 1280x1024 --same-as LVDS1 --rate 75
 
 #Forcing new resolution
 #cvt 1280 1024
+#cvt -r 1280 1024
 #xrandr --newmode "1280x1024_60.00"  109.00  1280 1368 1496 1712  1024 1027 1034 1063 -hsync +vsync
 #xrandr --addmode VGA1 1280x1024_60.00
 #xrandr --output VGA --mode 1280x1024_60.00

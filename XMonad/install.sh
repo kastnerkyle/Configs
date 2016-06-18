71eb0749d027492be1a609e9a272a33bf74fb785
Kyle Kastner <kastnerkyle@gmail.com>
2016-06-17 22:14:59 -0700

    Added screenrc

--- XMonad/install.sh
+++ XMonad/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt-get install -y xmonad xmobar suckless-tools trayer xclip openssh-server screen valgrind vim scrot flashplugin-installer xbacklight
+sudo apt-get install -y xmonad xmobar suckless-tools trayer xclip openssh-server screen vim scrot
 
 CUR_DIR=`pwd`
 mkdir ~/.xmonad
 ln -sf $CUR_DIR/xmonad.hs $HOME/.xmonad/xmonad.hs
 ln -sf $CUR_DIR/xmobarrc $HOME/.xmobarrc
+ln -sf $CUR_DIR/screenrc $HOME/.screenrc

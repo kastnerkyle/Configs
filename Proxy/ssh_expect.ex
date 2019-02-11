f5dd4f9aa6890efe7a915af7360991628c17e2cd
Kyle Kastner <kastnerkyle@gmail.com>
2019-02-11 09:56:42 -0500

    Added port arg

--- Proxy/ssh_expect.ex
+++ Proxy/ssh_expect.ex
@@ -2,12 +2,12 @@
 # Example aliases:
 #alias ssh_to_place="./ssh_login_expect.ex username@place"
 #!/usr/bin/expect
-set timeout 20
+set timeout 45
 
 set passwd $::env(SSH_EXPECT_PW)
 set postflags [lrange $argv 0 end]
 
-spawn ssh {*}$postflags
-expect "password:"
+spawn ssh -p 8002 {*}$postflags
+expect "Password:"
 send "$passwd\r";
 interact
